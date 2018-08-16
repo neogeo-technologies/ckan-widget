95b81613400cd90fc062d96bdc9ea1530c7ab07a
Zoran Pandovski <zoran.pandovski@keitaro.com>
2018-08-16 11:54:14 +0200

    Add bootstrap 4

--- ckan-widget/src/index.js
+++ ckan-widget/src/index.js
@@ -7,6 +7,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 const store = createStoreWithMiddleware(reducers);
