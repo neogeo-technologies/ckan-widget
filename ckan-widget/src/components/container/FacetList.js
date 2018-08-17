9e7b700977bab09aa807d14d135eec5fc0bf1091
Zoran Pandovski <zoran.pandovski@keitaro.com>
2018-08-17 17:10:54 +0200

    Call pkg search on facets click

--- ckan-widget/src/components/container/FacetList.js
+++ ckan-widget/src/components/container/FacetList.js
@@ -1,24 +1,46 @@
-import React, { Component } from 'react'
-import Facet from '../presentational/Facet'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import Facet from "../presentational/Facet";
+import * as actions from "../../actions";
 
 //Default ckan facets
 const FACETS_TITLE = {
-    'organization': 'Organizations',
-    'groups': 'Groups',
-    'tags': 'Tags',
-    'res_format': 'Formats',
-    'license_id': 'Licenses',
-}
+  organization: "Organizations",
+  groups: "Groups",
+  tags: "Tags",
+  res_format: "Formats",
+  license_id: "Licenses"
+};
 
-export default class FacetList extends Component{
+class FacetList extends Component {
 
-    render(){
-
-        let facetList = Object.values(FACETS_TITLE).map((ft, i) => {
-            return <Facet  title={ft} key={i} />
-        });
-
-        return facetList;
-
+  componentDidMount() {
+    this.props.packageSearch();
+  }
+    onClick = (data) => {
+      this.props.packageSearch({q: data})
     }
+
+  render() {
+    const { facets } = this.props;
+    const facetsList = [];
+    for(let i in facets) {
+      let title = FACETS_TITLE[i];
+      let facetsInfo = facets[i];
+
+      facetsList.push(<Facet title={title} key={i} facetsInfo={facetsInfo.items} facetKey={i} onClick={this.onClick} />);
+    }
+    return facetsList;
+  }
 }
+const mapStateToProps = state => {
+  return {
+    facets: state.packageSearch.facets
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  actions
+)(FacetList);
