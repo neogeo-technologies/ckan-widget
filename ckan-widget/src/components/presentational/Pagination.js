f2d6c2e1e7f94f8d031e8a2a01467e57554a05d8
Zoran Pandovski <zoran.pandovski@keitaro.com>
2018-08-20 13:47:51 +0200

    Lint code

--- ckan-widget/src/components/presentational/Pagination.js
+++ ckan-widget/src/components/presentational/Pagination.js
@@ -1,29 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import ReactPaginate from 'react-paginate';
 
 
 class Pagination extends Component {
   render() {
-    const { page, pageCount } = this.props
+    const { page, pageCount } = this.props;
     if (pageCount > 1) {
-        return (
-            <ReactPaginate previousLabel={"<<"}
-                nextLabel={">>"}
-                breakLabel={'...'}
-                breakClassName={"break-me"}
-                forcePage={page}
-                pageCount={pageCount}
-                marginPagesDisplayed={2}
-                pageRangeDisplayed={5}
-                onPageChange={ data => this.props.handlePagination(data.selected) }
-                containerClassName={"pagination"}
-                subContainerClassName={"pages pagination"}
-                activeClassName={"active"} />
-        )
+      return (
+        <ReactPaginate
+          previousLabel={'<<'}
+          nextLabel={'>>'}
+          breakLabel="..."
+          breakClassName="break-me"
+          forcePage={page}
+          pageCount={pageCount}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={5}
+          onPageChange={data => this.props.handlePagination(data.selected)}
+          containerClassName="pagination"
+          subContainerClassName="pages pagination"
+          activeClassName="active"
+        />
+      );
     }
 
-    return(null)
+    return (null);
   }
 }
 
-export default Pagination
+export default Pagination;
--- ckan-widget/src/index.js
+++ ckan-widget/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import { render } from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import registerServiceWorker from './registerServiceWorker';
+import App from './App';
 import reducers from './reducers';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -13,10 +13,10 @@ const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 const store = createStoreWithMiddleware(reducers);
 
 render(
-    <Provider store={store}>
-        <App/>
-    </Provider>,
-    document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
 );
 
 registerServiceWorker();
