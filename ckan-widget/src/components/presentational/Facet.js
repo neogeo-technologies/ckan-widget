97188e660c2acd6b17460b535d167a30464fac15
Zoran Pandovski <zoran.pandovski@keitaro.com>
2018-08-23 14:53:01 +0200

    Initial facet list

--- ckan-widget/src/components/presentational/Facet.js
+++ ckan-widget/src/components/presentational/Facet.js
@@ -5,13 +5,14 @@ class Facet extends Component {
   render() {
     const facets = this.props.facetsInfo;
     const title = this.props.title;
+    console.log(this.props + 'OD FACETS')
     const infoList = [];
-    for (const f in facets) {
+    for (const facetId in facets) {
       infoList.push(
-        <li className="list-group-item d-flex justify-content-between align-items-center" key={f} onClick={() => this.props.onClick(`${this.props.facetKey}:${facets[f].name}`)}>
-          {facets[f].display_name}
+        <li className="list-group-item d-flex justify-content-between align-items-center" key={facetId} onClick={() => this.props.onClick(`${this.props.facetKey}:${facets[facetId].name}`)}>
+          {facets[facetId].display_name}
           {' '}
-          <span className="badge badge-primary">{facets[f].count}</span>
+          <span className="badge badge-primary">{facets[facetId].count}</span>
         </li>,
       );
     }
