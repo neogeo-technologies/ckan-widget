c95bb3df62f20cff55b33fa094f65eb5597b801d
Vladimir Tomanovski <vladoohr@gmail.com>
2018-08-23 11:55:16 +0200

    Tests Error component

--- ckan-widget/src/__test__/components/presentational/DatasetDetails.test.js
+++ ckan-widget/src/__test__/components/presentational/DatasetDetails.test.js
@@ -8,7 +8,14 @@ describe('DatasetDetails', () => {
   });
   it('should render correctly', () => {
     const tree = shallow(
-      <DatasetDetails organization={{ name: 'org1' }} resources={[{ restricted: "{\"level\": \"public\"}", format: 'CSV', name: 'rsc1', last_modified: '21 June 2018' }]}  />
+      <DatasetDetails
+        name={'Dataset1'}
+        notes={'Describe dataset1'}
+        dataset_creation_date={'2018-03-30'}
+        dataset_modification_date={'2018-05-30'}
+        dataset_publication_date={'2018-04-30'}
+        organization={{ name: 'org1' }}
+        resources={[{ restricted: "{\"level\": \"public\"}", format: 'CSV', name: 'rsc1', last_modified: '21 June 2018' }]} />
     );
     expect(tree).toMatchSnapshot();
   });
