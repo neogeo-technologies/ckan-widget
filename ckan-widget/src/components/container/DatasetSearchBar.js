3b46f6023a28e7295146f004c446ce6ea896c7a0
Zoran Pandovski <zoran.pandovski@keitaro.com>
2018-08-23 14:52:27 +0200

    Fix search

--- ckan-widget/src/components/container/DatasetSearchBar.js
+++ ckan-widget/src/components/container/DatasetSearchBar.js
@@ -3,27 +3,33 @@ import { connect } from 'react-redux'
 
 import SearchBar from '../presentational/SearchBar'
 import * as actions from '../../actions'
+import SelectedFacetList from '../container/SelectedFacetList'
 
 class DatasetSearchBar extends Component{
    handleInputChange = (event, value) => {
       if (event) {
         event.preventDefault()
       }
-      const { sort, rows } = this.props
-      this.props.packageSearch({ q: value, rows: rows, sort: sort })
+      const { sort, rows, facet_search } = this.props
+      this.props.packageSearch({ q: value, rows: rows, sort: sort, fq: facet_search })
     }
 
     render(){
         return(
-          <SearchBar handleInputChange={this.handleInputChange}/>
+            <div>
+                <SearchBar handleInputChange={this.handleInputChange}/>
+                <SelectedFacetList sort={this.props.sort} rows={this.props.rows} selected_facets={this.props.facet_search}/>
+            </div>
         )
     }
 }
 
 const mapStateToProps = state => {
   return {
+    search: state.packageSearch.search,
     rows: state.packageSearch.rows,
-    sort: state.packageSearch.sort
+    sort: state.packageSearch.sort,
+    facet_search: state.packageSearch.facet_search
   }
 }
 
