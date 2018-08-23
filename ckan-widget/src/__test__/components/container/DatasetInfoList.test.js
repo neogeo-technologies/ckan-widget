ffbb4d8c261ba0961eb971569d828b514df68128
Vladimir Tomanovski <vladoohr@gmail.com>
2018-08-23 23:46:47 +0200

    Properly passed store to DatasetInfoList component in tests

--- ckan-widget/src/__test__/components/container/DatasetInfoList.test.js
+++ ckan-widget/src/__test__/components/container/DatasetInfoList.test.js
@@ -1,32 +1,31 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import configureMockStore from 'redux-mock-store';
-import { Provider } from 'react-redux'
+import  configureStore from 'redux-mock-store';
 import DatasetInfoList from '../../../components/container/DatasetInfoList';
 
 
 let component;
 let store;
-const mockStore = configureMockStore();
+const mockStore = configureStore();
 const initialState = {
-  datasets: [],
-  search: '',
-  rows: 10,
-  facets: [],
-  total: 20,
-  page: 1,
-  sort: 'score desc, metadata_modified desc',
-  error: '',
-  facet_search: ''
+  packageSearch: {
+    datasets: [],
+    search: '',
+    rows: 10,
+    facets: [],
+    total: 20,
+    page: 1,
+    sort: 'score desc, metadata_modified desc',
+    error: '',
+    facet_search: ''
+  }
 };
 
 describe('DatasetInfoList', () => {
   beforeEach(() => {
     store = mockStore(initialState);
     component = shallow(
-      <Provider store={store}>
-        <DatasetInfoList />
-      </Provider>
+        <DatasetInfoList store={store} />
     );
   })
 
