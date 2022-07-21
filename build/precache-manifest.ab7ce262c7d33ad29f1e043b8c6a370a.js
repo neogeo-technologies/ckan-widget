f3f0e7fc042c192e538982f429dde90743d6f4fa
m431m <mmeliani@neogeo.fr>
2022-07-21 15:28:25 +0200

    version 0.1.3 && build

--- build/precache-manifest.ab7ce262c7d33ad29f1e043b8c6a370a.js
+++ build/precache-manifest.ab7ce262c7d33ad29f1e043b8c6a370a.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dda046a35c63413c9849ae8422071e7c",
+    "revision": "6ac2285e8389444f83642a0a9e9edc18",
     "url": "./index.html"
   },
   {
-    "revision": "3afa8bea0c13cc992fed",
+    "revision": "1e1190da6a47449d8f8e",
     "url": "./static/css/main.4d1cfba5.chunk.css"
   },
   {
-    "revision": "2d6571137a8cb4cf9ab1",
-    "url": "./static/js/2.be3099fe.chunk.js"
+    "revision": "992a7692098c5f78a4b7",
+    "url": "./static/js/2.85a3e85f.chunk.js"
   },
   {
-    "revision": "3afa8bea0c13cc992fed",
-    "url": "./static/js/main.f96a2322.chunk.js"
+    "revision": "1e1190da6a47449d8f8e",
+    "url": "./static/js/main.404aabed.chunk.js"
   },
   {
     "revision": "18c578c871504ee60207",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.3e39db0df69361f93699d8ce7a1ce3b1.js"
+  "./precache-manifest.ab7ce262c7d33ad29f1e043b8c6a370a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
